1e22697941c235303e208a7cddffb3f7e0540308
SMSazzad <sazzadcse092@gmail.com>
2019-06-28 15:14:34 +0600

    assignment_05_added

---
+++ RefrigeratorAppPractice3/RefrigeratorAppPractice3/Manage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefrigeratorAppPractice3
+{
+    class Manage
+    {
+
+        public double Limit { get; set; }
+
+        private double loadedWeight = 0.0;
+
+        public bool AddWeight(int items, double weight)
+        {
+            double chk = items * weight;
+            if (loadedWeight + chk <= Limit)
+            {
+                loadedWeight += chk;
+                return true;
+            }
+
+            else
+                return false;
+            
+        }
+
+        public double CurrentWeight()
+        {
+            return loadedWeight;
+        }
+        public double RemainingWeight()
+        {
+            return Limit - loadedWeight;
+        }
+
+
+    }
+}
---
+++ RefrigeratorAppPractice3/RefrigeratorAppPractice3/RefrigeratorUi.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RefrigeratorAppPractice3
+{
+    public partial class RefrigeratorUi : Form
+    {
+        public RefrigeratorUi()
+        {
+            InitializeComponent();
+        }
+
+        Manage manage = new Manage();
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            if(String.IsNullOrEmpty(maxWeightTakeTextBox.Text))
+            {
+                maxWeightTakeTextBox.Text = "Enter max weight ";
+                return;
+            }
+            double limit = Convert.ToDouble(maxWeightTakeTextBox.Text);
+            manage.Limit = limit;
+
+        }
+
+        private void EnterButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                int numberItems = Convert.ToInt32(itemTextBox.Text);
+                double weight = Convert.ToDouble(weightTextBox.Text);
+                
+                if (! manage.AddWeight(numberItems, weight))
+                {
+                    MessageBox.Show("Your given amount can’t be entered because it will be over flown.");
+                    return;
+                }
+                else
+                {
+                    currentWeightTextBox.Text = Convert.ToString(manage.CurrentWeight());
+                    remainingWeightTextBox.Text = Convert.ToString(manage.RemainingWeight());
+                }
+                
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+
+
+        }
+    }
+}
